ae6209edbe6ef8faef13b50525dceb6057a01044
hft <15766476597@163.com>
2021-10-16 09:21:49 +0800

    添加README

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
  * @Author: hft
  * @Date: 2021-10-13 15:55:27
  * @LastEditors: hft
- * @LastEditTime: 2021-10-15 15:13:15
+ * @LastEditTime: 2021-10-15 17:25:07
  * @Description: file content
  */
 import React from 'react';
@@ -24,7 +24,7 @@ const initialState: IState = {
   token: null,
 };
 // createContext要有初始值，可以不用再约束类型，会根据初始值识别类型
-export const AuthContext = React.createContext<any>(initialState)
+export const AuthContext = React.createContext<IState | any>(initialState)
 
 function App() {
   const [state, dispatch]: any = React.useReducer<any>(appReducer, initialState)
@@ -40,7 +40,7 @@ function App() {
         }
       })
     }
-    console.log("isAuthenticated",state.isAuthenticated);
+    console.log("isAuthenticated", state.isAuthenticated);
   }, [])
   return (
     <AuthContext.Provider
@@ -49,7 +49,7 @@ function App() {
         dispatch
       }}
     >
-      
+
       <Header />
       <div className="App">{!state.isAuthenticated ? <Login /> : <Home />}</div>
     </AuthContext.Provider>
